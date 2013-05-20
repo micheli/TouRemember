dd96c45ee7b30faf696472615a6c38d35253fc12
micheli <micheli.mik@gmail.com>
2013-05-20 15:01:45 +0200

    add GPStoMeter

---
+++ src/com/example/tablet/GPSToMeter.java
@@ -0,0 +1,25 @@
+package com.example.tablet;
+
+import android.util.FloatMath;
+
+public class GPSToMeter{
+	public double gps2m (float lat_a, float lng_a, float lat_b, float lng_b) {
+		float pk = (float) (180 / 3.14169);
+
+		float a1 = lat_a / pk;
+		float a2 = lng_a / pk;
+		float b1 = lat_b / pk;
+		float b2 = lng_b / pk;
+
+		float t1 = FloatMath.cos(a1) * FloatMath.cos(a2) * FloatMath.cos(b1)
+				* FloatMath.cos(b2);
+		float t2 = FloatMath.cos(a1) * FloatMath.sin(a2) * FloatMath.cos(b1)
+				* FloatMath.sin(b2);
+		float t3 = FloatMath.sin(a1) * FloatMath.sin(b1);
+		double tt = Math.acos(t1 + t2 + t3);
+
+		return 6366000 * tt;
+	}
+	
+	
+}
