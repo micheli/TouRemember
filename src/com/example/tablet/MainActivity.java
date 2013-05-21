fa59cef98aa29a0fbca37b1be0cb2419f53d5aea
micheli <micheli.mik@gmail.com>
2013-05-21 15:24:22 +0200

    èp gia su

--- src/com/example/tablet/MainActivity.java
+++ src/com/example/tablet/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.tablet;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -9,7 +13,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,6 +38,7 @@ public class MainActivity extends Activity {
 	LatLng myPosition;
 	LocationManager mlocManager;
 	public LatLng lastPos;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -86,7 +93,6 @@ public class MainActivity extends Activity {
 
 	/* Class My Location Listener */
 	public class MyLocationListener implements LocationListener {
-		//public LatLng lastPos;
 
 		@Override
 		public void onLocationChanged(Location loc) {
@@ -126,32 +132,6 @@ public class MainActivity extends Activity {
 							}
 						}
 					});
-			// Foto
-			//
-//			findViewById(R.id.foto).setOnClickListener(new OnClickListener() {
-//				private Uri fileUri;
-//				// public static final int MEDIA_TYPE_IMAGE = 1;
-//				private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-//
-//				@Override
-//				public void onClick(View v) {
-//					// create Intent to take a picture and return control to the
-//					// calling application
-//					Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//
-//					// fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); //
-//					// create a file to save the image
-//					intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-//					// set the image file name
-//
-//					// start the image capture Intent
-//					startActivityForResult(intent,
-//							CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-//					mMap.addMarker(new MarkerOptions().position((lastPos))
-//							.title("foto"));
-//
-//				}
-//			});
 		}
 
 		@Override
@@ -187,31 +167,76 @@ public class MainActivity extends Activity {
 		if (LocationChange) {
 			switch (item.getItemId()) {
 			case R.id.take_photo:
-				//Uri fileUri;
-				// public static final int MEDIA_TYPE_IMAGE = 1;
+				Uri fileUri;
+				final int MEDIA_TYPE_IMAGE = 1;
 				final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
+				
+				
 				// create Intent to take a picture and return control to the
 				// calling application
 				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-				// fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); //
+				fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); //
 				// create a file to save the image
-				// intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+				intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
 				// set the image file name
 
 				// start the image capture Intent
 				startActivityForResult(intent,
 						CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 
-				mMap.addMarker(new MarkerOptions().position((lastPos))
-						.title("foto"));
+				mMap.addMarker(new MarkerOptions().position((lastPos)).title(
+						"foto"));
 
 				return true;
 			}
-			
+
 		}
 
 		return false;
+	}
+	
+	public static final int MEDIA_TYPE_IMAGE = 1;
+
+
+	/** Create a file Uri for saving an image or video */
+	private static Uri getOutputMediaFileUri(int type){
+	      return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type){
+	    // To be safe, you should check that the SDCard is mounted
+	    // using Environment.getExternalStorageState() before doing this.
+
+	    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+	              Environment.DIRECTORY_PICTURES), "MyCameraApp");
+	    // This location works best if you want the created images to be shared
+	    // between applications and persist after your app has been uninstalled.
+
+	    // Create the storage directory if it does not exist
+	    if (! mediaStorageDir.exists()){
+	        if (! mediaStorageDir.mkdirs()){
+	            Log.d("MyCameraApp", "failed to create directory");
+	            return null;
+	        }
+	    }
+
+	    // Create a media file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    File mediaFile;
+	    if (type == MEDIA_TYPE_IMAGE){
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+	        "IMG_"+ timeStamp + ".jpg");}
+//	    } else if(type == MEDIA_TYPE_VIDEO) {
+//	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+//	        "VID_"+ timeStamp + ".mp4");
+//	    } 
+	        else {
+	        return null;
+	    }
+
+	    return mediaFile;
 	}
 }
